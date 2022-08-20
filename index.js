7eae71d2184c976bc23bb7f962a7c55a4903ac23
Duncan Cheruiyot Langat <duncan.langat@student.moringaschool.com>
2022-08-20 12:47:57 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+function  receivesAFunction(callback){
+    callback();
+}
+
+function returnsANamedFunction(){
+    return function callback(){
+
+    }
+
+}
+function returnsAnAnonymousFunction(){
+    return function(){
+
+    }
+}
+
+
